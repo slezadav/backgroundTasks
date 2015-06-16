0faeb6c168b0bb6abf8dc165f8f98ad72f1f60ca
david.slezak <david.slezak@firma.seznam.cz>
2015-06-16 06:52:18 +0200

    Improved testing and dependencies

--- sample/src/androidTest/java/com/example/backgroundtaskstest/BackgroundTasksTest.java
+++ sample/src/androidTest/java/com/example/backgroundtaskstest/BackgroundTasksTest.java
@@ -1,13 +1,9 @@
 package com.example.backgroundtaskstest;
 
-import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.ActivityUnitTestCase;
-import android.test.suitebuilder.annotation.MediumTest;
-import android.widget.Button;
-import com.robotium.solo.Solo;
+import android.util.Log;
 
-import junit.framework.TestCase;
+import com.robotium.solo.Solo;
 
 /**
  * Created by Davo on 14.6.2015.
@@ -26,8 +22,20 @@ public class BackgroundTasksTest
         solo = new Solo(getInstrumentation(), getActivity());
     }
 
+    public void testCase(){
+        singleAsync();
+        singleAsyncCancel();
+        singleAsyncService();
+        singleAsyncCancelService();
+        chainAsync();
+        chainAsyncCancel();
+        chainAsyncService();
+        chainAsyncCancelService();
+    }
 
-    public void testSingleAsync() {
+
+    public void singleAsync() {
+        Log.d("BackgroundTasksTest", "singleAsync");
         solo.setActivityOrientation(Solo.PORTRAIT);
         solo.waitForActivity(MainActivity.class,
                 5000);
@@ -38,7 +46,8 @@ public class BackgroundTasksTest
         assertTrue("Task did not succeed",solo.waitForLogMessage("onTaskSuccess task",5000));
     }
 
-    public void testChainAsync() {
+    public void chainAsync() {
+        Log.d("BackgroundTasksTest", "chainAsync");
         solo.setActivityOrientation(Solo.PORTRAIT);
         solo.clickOnButton("Chain");
         assertTrue("Chain did not publish progress",solo.waitForLogMessage("onTaskProgress chain",3000));
@@ -54,7 +63,8 @@ public class BackgroundTasksTest
         assertTrue("Chain did not succeed",solo.waitForLogMessage("onTaskSuccess chain",10000));
     }
 
-    public void testSingleAsyncCancel() {
+    public void singleAsyncCancel() {
+        Log.d("BackgroundTasksTest", "singleAsyncCancel");
         solo.setActivityOrientation(Solo.PORTRAIT);
         solo.clickOnButton("Task");
         assertTrue("Task not started",solo.waitForLogMessage("onTaskReady task",1000));
@@ -63,7 +73,8 @@ public class BackgroundTasksTest
         assertTrue("Task did not cancel",solo.waitForLogMessage("onTaskCancel task",3000));
     }
 
-    public void testChainAsyncCancel() {
+    public void chainAsyncCancel() {
+        Log.d("BackgroundTasksTest", "chainAsyncCancel");
         solo.setActivityOrientation(Solo.PORTRAIT);
         solo.clickOnButton("Chain");
         solo.sleep(1000);
@@ -72,7 +83,8 @@ public class BackgroundTasksTest
         assertTrue("Chain did not cancel",solo.waitForLogMessage("onTaskCancel chain",3000));
     }
 
-    public void testSingleAsyncService() {
+    public void singleAsyncService() {
+        Log.d("BackgroundTasksTest", "singleAsyncService");
         solo.setActivityOrientation(Solo.PORTRAIT);
         solo.waitForActivity(MainActivity.class,
                 5000);
@@ -83,7 +95,8 @@ public class BackgroundTasksTest
         assertTrue("Task did not succeed",solo.waitForLogMessage("onTaskSuccess task",5000));
     }
 
-    public void testChainAsyncService() {
+    public void chainAsyncService() {
+        Log.d("BackgroundTasksTest", "chainAsyncService");
         solo.setActivityOrientation(Solo.PORTRAIT);
         solo.clickOnButton("Chain Service");
         assertTrue("Chain did not publish progress",solo.waitForLogMessage("onTaskProgress chain",3000));
@@ -99,7 +112,8 @@ public class BackgroundTasksTest
         assertTrue("Chain did not succeed",solo.waitForLogMessage("onTaskSuccess chain",10000));
     }
 
-    public void testSingleAsyncCancelService() {
+    public void singleAsyncCancelService() {
+        Log.d("BackgroundTasksTest", "singleAsyncCancelService");
         solo.setActivityOrientation(Solo.PORTRAIT);
         solo.clickOnButton("Task Service");
         assertTrue("Task not started",solo.waitForLogMessage("onTaskReady task",1000));
@@ -108,7 +122,8 @@ public class BackgroundTasksTest
         assertTrue("Task did not cancel",solo.waitForLogMessage("onTaskCancel task",3000));
     }
 
-    public void testChainAsyncCancelService() {
+    public void chainAsyncCancelService() {
+        Log.d("BackgroundTasksTest", "chainAsyncCancelService");
         solo.setActivityOrientation(Solo.PORTRAIT);
         solo.clickOnButton("Chain Service");
         solo.sleep(1000);
